9fa87950ccfa28a99f3ca403e09860a1c6e7a74a
dov liberman <doliberm@g.jct.ac.il>
2023-08-29 10:38:51 +0300

    delete eye detector cv2 keep face detector cv2

--- Sleep_detector.py
+++ Sleep_detector.py
@@ -3,10 +3,7 @@ import os
 
 cascades_dir = os.path.join(os.getcwd(), 'cascades')
 face_Cascade_dir = os.path.join(cascades_dir, 'haarcascade_frontalface_default.xml')
-eye_Cascade_dir = os.path.join(cascades_dir, 'haarcascade_eye.xml')
-
 face_cascade = cv2.CascadeClassifier(face_Cascade_dir)
-eye_cascade = cv2.CascadeClassifier(eye_Cascade_dir)
 
 
 def detect_face(img):
@@ -14,41 +11,6 @@ def detect_face(img):
     face_rect = face_cascade.detectMultiScale(face_img, scaleFactor=1.2, minNeighbors=5)
 
     return face_rect
-
-
-def detect_eyes_in_face_img(face_img):
-    face_img = face_img.copy()
-    eyes_rect = eye_cascade.detectMultiScale(face_img, scaleFactor=1.2, minNeighbors=9)
-    return eyes_rect
-
-
-def eye_detector(img):
-    resize_img = cv2.resize(img, (1000, 600))
-
-    faces_rect = detect_face(resize_img)
-
-    big_face_rect = max(faces_rect, key=lambda f: f[2])
-
-    x, y, w, h = big_face_rect
-
-    face_img = resize_img[y:y + h, x:x + w, :]
-
-    eyes_rects = detect_eyes_in_face_img(face_img)
-    show_img(resize_img)
-    show_img(face_img)
-
-
-    for (x, y, w, h) in eyes_rects:
-       cv2.rectangle(face_img, (x, y), (x + w, y + h), (255, 0, 0), 10)
-
-    show_img(face_img)
-
-
-   # eyes_images = [ face_img[y:y + h, x:x + w, :] for x, y, w, h in eyes_rects]
-
-  #  return eyes_images
-
-
 
 
 def show_img(img):
@@ -60,7 +22,4 @@ def show_img(img):
 
 
 if __name__ == '__main__':
-    img = cv2.imread(r'C:\Users\dov31\Desktop\smash\IMG_0153.JPG')
-
-    eye_detector(img)
-
+    img = cv2.imread(r'your image')
