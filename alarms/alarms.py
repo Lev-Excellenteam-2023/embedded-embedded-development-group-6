d2fae2f4140af8b5e118963b10dabcaa1f7356b1
Yisrael jacob <yisraeljakob@gmail.com>
2023-08-29 17:07:29 +0300

    alarmdoes not work

--- alarms/alarms.py
+++ alarms/alarms.py
@@ -1,11 +1,12 @@
 import time
 import playsound
+from utils.consts import ALARM_PATH
 
 
 def alarm():
     time1 = time.time() + 1
     while time1 > time.time():
-        playsound.playsound('Alarm.mp3')
+        playsound.playsound(ALARM_PATH)
 
 
 if __name__ == '__main__':
--- utils/consts.py
+++ utils/consts.py
@@ -1,6 +1,9 @@
-
+import os
 PREDICTOR_PATH = 'blink_detector\\shape_predictor_68_face_landmarks.dat'
 
 EYE_AR_THRESH = 0.22
-MAX_BLINKS = 5
+MAX_BLINKS = 2
+
+ALARM_PATH = r'alarms\Alarm.mp3'#os.path.join(os.getcwd(), os.pardir, 'alarms','Alarm.mp3')
+print(ALARM_PATH)
 
