fbbc03a57794d655bcf4b45cff05045922b4980f
Yisrael jacob <yisraeljakob@gmail.com>
2023-08-30 12:08:30 +0300

    Deleted dots from path

--- utils/consts.py
+++ utils/consts.py
@@ -4,13 +4,13 @@ from os.path import join, abspath
 
 FONT = FONT_HERSHEY_SIMPLEX
 
-PREDICTOR_PATH = join(__file__, pardir, pardir, 'blink_detector', 'shape_predictor_68_face_landmarks.dat')
+PREDICTOR_PATH = abspath(join(__file__, pardir, pardir, 'blink_detector', 'shape_predictor_68_face_landmarks.dat'))
 
 EYE_AR_THRESH = 0.22
 MAX_BLINKS = 2
 
 ALARM_PATH = abspath(join(__file__, pardir, pardir, 'alarms', 'Alarm.mp3'))
-    
+
 GREEN = (0, 255, 0)
 WIDTH_RESIZE = 500
 PUT_TEXT_THICKNESS = 2
